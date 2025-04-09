e0476b23a7c719fcd9afffa9fd7f3f0cf10a3670
Nathan Ling <32849838+lingnath@users.noreply.github.com>
2025-04-09 16:27:38 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install python3.10 -y
+sudo apt-get install pip -y
+sudo apt install python3.10-venv
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+pip install notebook==7.4.0
+jupyter notebook
